c0d127f8b0f111e72d84e4dd66a5278d3e0ecd5f
内藤広貴 <uchifujihirotaka@uchifujihirotakanoMacBook-Pro.local>
2024-01-03 17:48:13 +0900

    chage url

--- ios-stations/BookAPIClient.swift
+++ ios-stations/BookAPIClient.swift
@@ -12,9 +12,18 @@ protocol BookAPIClientProtocol {
 
 class BookAPIClient: BookAPIClientProtocol {
     func fetchBooks(offset: Int, completion: @escaping ([Book]?) -> Void) {
-        let url = "https://railway.bookreview.tech/books"
+        let url = "https://railway.bookreview.techtrain.dev/public/books"
         AF.request(url).response { response in
-            guard let data = response.data else {
+            print("Received data: \(String(data: response.data ?? Data(), encoding: .utf8) ?? "Invalid data")")
+
+            guard response.error == nil else {
+                print("Error: \(response.error!.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            guard let data = response.data, let httpResponse = response.response, httpResponse.statusCode == 200 else {
+                print("Error: Invalid response or status code.")
                 completion(nil)
                 return
             }
@@ -23,7 +32,7 @@ class BookAPIClient: BookAPIClientProtocol {
                 let books = try JSONDecoder().decode([Book].self, from: data)
                 completion(books)
             } catch {
-                print(error.localizedDescription)
+                print("Error: \(error.localizedDescription)")
                 completion(nil)
             }
         }
