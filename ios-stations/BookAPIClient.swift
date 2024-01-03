cfad68a1ff308db8e7518c1454a8d6c6e8fac2a4
内藤広貴 <uchifujihirotaka@uchifujihirotakanoMacBook-Pro.local>
2024-01-03 17:23:58 +0900

    update BoolAPIClient

--- ios-stations/BookAPIClient.swift
+++ ios-stations/BookAPIClient.swift
@@ -2,6 +2,8 @@
 //  BookAPIClient.swift
 //  ios-stations
 //
+import Alamofire
+import Foundation
 
 protocol BookAPIClientProtocol {
     func fetchBooks(offset: Int, completion: @escaping ([Book]?) -> Void)
@@ -9,6 +11,20 @@ protocol BookAPIClientProtocol {
 
 class BookAPIClient: BookAPIClientProtocol {
     func fetchBooks(offset: Int, completion: @escaping ([Book]?) -> Void) {
-        completion(nil)
+        let url = "https://railway.bookreview.tech/books"
+        AF.request(url).response { response in
+            guard let data = response.data else {
+                completion(nil)
+                return
+            }
+            
+            do {
+                let books = try JSONDecoder().decode([Book].self, from: data)
+                completion(books)
+            } catch {
+                print(error.localizedDescription)
+                completion(nil)
+            }
+        }
     }
 }
--- ios-stations/FirstViewController.swift
+++ ios-stations/FirstViewController.swift
@@ -8,36 +8,35 @@ import UIKit
 class FirstViewController: UIViewController {
 
     var books: [Book]?
+    let apiClient: BookAPIClientProtocol = BookAPIClient()
     
-    // ボタンのUIと紐付け
-    @IBOutlet weak var presentSecondViewController: UIButton!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        // self.view.backgroundColor = UIColor.Theme.main
-    }
-    // ボタンを押した時の処理
-    @IBAction func presentSecondViewController(_ sender: UIButton) {
-        let secondViewController = SecondViewController(url: "https://techbowl.co.jp/")
-        secondViewController.modalPresentationStyle = .fullScreen
-        self.present(secondViewController, animated: true, completion: nil)
+        fetchBooks()
     }
     
-    //    @IBAction func tappedButton(_ sender: UIButton) {
-//        tapButton.backgroundColor = UIColor.random
-//    }
+    private func fetchBooks() {
+        apiClient.fetchBooks(offset: 0) { [weak self] books in
+            guard let books = books else { return }
+            self?.books = books
+            self?.tableView.reloadData()
+        }
+    }
     
 }
 
 extension FirstViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseCell", for: indexPath) as! BookCell
+        let book = books?[indexPath.row]
+        // セルの設定
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return books?.count ?? 0
     }
 }
 
