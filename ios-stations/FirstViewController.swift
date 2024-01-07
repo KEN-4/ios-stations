7bb784a02c3a96f2f4a3c0ebeca3ed0b9f20dd5a
内藤広貴 <uchifujihirotaka@uchifujihirotakanoMacBook-Pro.local>
2024-01-07 20:33:16 +0900

    fix bug

--- ios-stations/FirstViewController.swift
+++ ios-stations/FirstViewController.swift
@@ -14,7 +14,6 @@ class FirstViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(BookCell.self, forCellReuseIdentifier: "reuseCell")
         fetchBooks()
         
     }
