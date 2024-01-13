dce85465b64c5fd8bc4b966f798fd6446251facf
内藤広貴 <uchifujihirotaka@uchifujihirotakanoMacBook-Pro.local>
2024-01-14 08:26:26 +0900

    clear station17

--- ios-stations/FirstViewController.swift
+++ ios-stations/FirstViewController.swift
@@ -7,32 +7,73 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
+    // 書籍データを保持する配列
     var books: [Book]?
+    
+    // API通信を行うためのクライアント
     let apiClient: BookAPIClientProtocol = BookAPIClient()
     
+    // TableViewとボタンのアウトレット（UI部品との接続）
     @IBOutlet weak var tableView: UITableView!
+    fileprivate let refreshCtl = UIRefreshControl()
+    @IBOutlet weak var fetchBooksButton: UIButton!
     
+    // ボタンをタップしたときに呼ばれるアクション
+    @IBAction func touchFetchBooksButton(_ sender: Any) {
+        fetchBooks()
+        print("Touched!")
+    }
+    
+    // ビューがロードされたときに呼ばれるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchBooks()
         
+        // TableViewにリフレッシュコントロールを追加
+        tableView.refreshControl = refreshCtl
+        refreshCtl.addTarget(self, action: #selector(fetchBooks), for: .valueChanged)
+        
     }
     
-    private func fetchBooks() {
+    // 書籍データを取得するメソッド
+    @objc private func fetchBooks() {
         apiClient.fetchBooks(offset: 0) { [weak self] books in
             guard let books = books else { return }
+            print("Fetched Books")
             self?.books = books
             self?.tableView.reloadData()
+            self?.refreshCtl.endRefreshing()
+        }
+    }
+    
+    // Segueの準備を行うメソッド
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SecondViewController" {
+            if let secondVC = segue.destination as? SecondViewController,
+               let indexPath = tableView.indexPathForSelectedRow,
+               let book = books?[indexPath.row] {
+                secondVC.url = book.url
+            }
+        }
+    }
+    
+    // ビューが表示される直前の動作を定義
+    override func viewWillAppear(_ animated: Bool) {
+        if let indexPath = tableView.indexPathForSelectedRow{
+            tableView.deselectRow(at: indexPath, animated: true)
         }
     }
     
 }
 
+// UITableViewDataSourceプロトコルに適合するための拡張
 extension FirstViewController: UITableViewDataSource {
+    // TableViewのセルの数を決定
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return books?.count ?? 0
     }
     
+    // 各セルの内容を設定
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseCell", for: indexPath) as? BookCell,
               let book = books?[indexPath.row] else {
@@ -44,18 +85,9 @@ extension FirstViewController: UITableViewDataSource {
     }
 }
 
+// UITableViewDelegateプロトコルに適合するための拡張
 extension FirstViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "SecondViewController", sender: self)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "SecondViewController" {
-            if let secondVC = segue.destination as? SecondViewController,
-               let indexPath = tableView.indexPathForSelectedRow,
-               let book = books?[indexPath.row] {
-                secondVC.url = book.url
-            }
-        }
     }
 }
--- ios-stations/SecondViewController.swift
+++ ios-stations/SecondViewController.swift
@@ -16,19 +16,24 @@ class SecondViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    // NSCoderを使った初期化
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
+    // ビューがロードされた後に呼び出されるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // WKWebViewを作成し、ビューコントローラのビューとして設定
         webView = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
         view = webView
         
+        // Webページの読み込みを開始
         load(withURL: url)
     }
     
+    // 指定されたURLのWebページを読み込むメソッド
     private func load(withURL urlString: String) {
         guard let url = URL(string: urlString) else { return }
         let request = URLRequest(url: url)
