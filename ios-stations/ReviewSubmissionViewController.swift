c9736af5bb92a35a84aca2fb9c35a6f494d38f3d
KEN-4 <ssstututusss617@gmail.com>
2024-02-08 18:37:37 +0900

    adapt validation

--- ios-stations/ReviewSubmissionViewController.swift
+++ ios-stations/ReviewSubmissionViewController.swift
@@ -2,22 +2,59 @@ import UIKit
 import Alamofire
 import KeychainAccess
 
-class ReviewSubmissionViewController: UIViewController {
+class ReviewSubmissionViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var uiLabel: UILabel!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var urlTextField: UITextField!
     @IBOutlet weak var detailTextField: UITextField!
-
     @IBOutlet weak var reviewTextField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        titleTextField.delegate = self
+        urlTextField.delegate = self
+        detailTextField.delegate = self
+        reviewTextField.delegate = self
+        
+        updateSubmitButtonState()
+    }
+    
+    //textFieldにテキストが入力されたり変更されたりするときに呼ばれるdelegate
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // ボタンの状態を更新
+        updateSubmitButtonState()
+        return true
+    }
+    
+    func updateSubmitButtonState() {
+        // 全てのTextFieldに入力があるか確認
+        // textがnillの場合,trueを返す→!で反転するからfalseを返す
+        let isTitleEntered = !(titleTextField.text?.isEmpty ?? true)
+        let isUrlEntered = !(urlTextField.text?.isEmpty ?? true)
+        let isDetailEntered = !(detailTextField.text?.isEmpty ?? true)
+        let isReviewEntered = !(reviewTextField.text?.isEmpty ?? true)
+        // ボタンの有効/無効を切り替え
+        submitButton.isEnabled = isTitleEntered && isUrlEntered && isDetailEntered && isReviewEntered
     }
     
     @IBAction func submitButtonTapped(_ sender: Any) {
-        // バリデーション
+        guard let name = titleTextField.text, name.isTitle() else {
+            self.uiLabel.text = "タイトルは3文字以上で入力してください"
+            return
+        }
+        guard let name = urlTextField.text, name.isURL() else {
+            self.uiLabel.text = "URLは3文字以上で入力してください"
+            return
+        }
+        guard let name = detailTextField.text, name.isDetail() else {
+            self.uiLabel.text = "詳細情報は3文字以上で入力してください"
+            return
+        }
+        guard let name = reviewTextField.text, name.isReview() else {
+            self.uiLabel.text = "レビューは3文字以上で入力してください"
+            return
+        }
         submitReview()
     }
     
--- ios-stations/Validation.swift
+++ ios-stations/Validation.swift
@@ -26,6 +26,31 @@ extension String {
         return matches.count > 0
     }
     
+    func isTitle() -> Bool {
+        let pattern = "^.{3,}$"
+        let matches = validate(str: self, pattern: pattern)
+        return matches.count > 0
+    }
+
+    func isURL() -> Bool {
+        let pattern = "^.{3,}$"
+        // let pattern = "^(http|https)://[\\w\\.\\-_]+\\.[\\w\\.\\-_]+.*$"
+        let matches = validate(str: self, pattern: pattern)
+        return matches.count > 0
+    }
+    
+    func isDetail() -> Bool {
+        let pattern = "^.{3,}$"
+        let matches = validate(str: self, pattern: pattern)
+        return matches.count > 0
+    }
+
+    func isReview() -> Bool {
+        let pattern = "^.{3,}$"
+        let matches = validate(str: self, pattern: pattern)
+        return matches.count > 0
+    }
+    
     private func validate(str: String, pattern: String) -> [NSTextCheckingResult] {
         guard let regex = try? NSRegularExpression(pattern: pattern) else { return [] }
         return regex.matches(in: str, range: NSRange(location: 0, length: str.count))
